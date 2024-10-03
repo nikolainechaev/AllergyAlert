f611bb5d7867b02a9f281bc81e99fc835d637f3d
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-10-03 15:50:08 -0400

    changed test to hit API directly

--- API/allergyAPITests/GeocodingServiceTests.cs
+++ API/allergyAPITests/GeocodingServiceTests.cs
@@ -1,48 +1,29 @@
-using Moq;
 using allergyAPI.Services;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
-using Moq.Protected;
-using System.Threading;
-using System.Net;
 
 public class GeocodingServiceTests
 {
     private readonly HttpClient _httpClient;
-    private readonly Mock<HttpMessageHandler> _httpMessageHandlerMock;
 
     public GeocodingServiceTests()
     {
-        _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
-        _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+        _httpClient = new HttpClient();
     }
 
     [Fact]
     public async Task GetGeocodingDataAsync_ReturnsCorrectResult()
     {
         // Arrange
-        var jsonResponse = "[{\"Latitude\": 40.7128, \"Longitude\": -74.0060}]";
-
-        _httpMessageHandlerMock.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(jsonResponse)
-            });
-
-        var geocodingService = new GeocodingService(_httpClient);
+        IGeocodingService geocodingService = new GeocodingService(_httpClient);
 
         // Act
         var result = await geocodingService.GetGeocodingDataAsync("New York", "US");
 
         // Assert
         Assert.NotNull(result);
-        Assert.Equal(40.7128, result?.Latitude);
-        Assert.Equal(-74.0060, result?.Longitude);
+        Assert.Equal(40.7127281, result?.Latitude);
+        Assert.Equal(-74.006015199999993, result?.Longitude);
     }
 }
